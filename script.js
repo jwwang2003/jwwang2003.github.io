153b8c2faf100580ac5a17bf718479df97f345c5
junweiwang <jwwang2003@gmail.com>
2020-05-07 12:14:24 -0700

    Update

--- script.js
+++ script.js
@@ -1,34 +1,24 @@
-var about = document.getElementById("about");
-about.onclick = loadAboutMe;
+(function () {
 
-function loadAboutMe() {
-    document.getElementById("content").innerHTML='<object type="text/html" data="aboutme.html" ></object>';
-}
+    var app = angular.module('sampleApp',['ngRoute']);
 
-var blog = document.getElementById("blog");
-blog.onclick = loadBlog;
-
-function loadBlog() {
-    document.getElementById("content").innerHTML='<object type="text/html" data="blog.html" ></object>';
-}
-
-var projects = document.getElementById("projects");
-projects.onclick = loadProjects;
-
-function loadProjects() {
-    document.getElementById("content").innerHTML='<object type="text/html" data="projects.html" ></object>';
-}
-
-var resume = document.getElementById("resume");
-resume.onclick = loadResume;
-
-function loadResume() {
-    document.getElementById("content").innerHTML='<object type="text/html" data="resume.html" ></object>';
-}
-
-var contact = document.getElementById("contact");
-contact.onclick = loadContact;
-
-function loadContact() {
-    document.getElementById("content").innerHTML='<object type="text/html" data="contact.html" ></object>';
-}
+    app.config(function ($routeProvider){
+        $routeProvider
+            .when('/aboutme',{
+                templateUrl:'aboutme.html'
+            })
+            .when('/blog', {
+                templateUrl:'blog.html'
+            })
+            .when('/projects', {
+                templateUrl:'projects.html'
+            })
+            .when('/resume', {
+                templateUrl:'resume.html'
+            })
+            .when('/contact', {
+                templateUrl:'contact.html'
+            })
+            .otherwise({ redirectTo:'/'});
+    });
+})();
