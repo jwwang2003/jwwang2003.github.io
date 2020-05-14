b34d67152ed302931763a0d37ab3b909f43d0847
junweiwang <@users.noreply.github.com>
2020-05-13 22:49:43 -0700

    Update

---
+++ covid19tracker/precache-manifest.1209d7f2e18e3e724584f38e743621e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da51bd2983c97e1952c88e156494754d",
+    "url": "/covid19tracker/index.html"
+  },
+  {
+    "revision": "5d15b95fcfc49524b019",
+    "url": "/covid19tracker/static/css/main.303d33ac.chunk.css"
+  },
+  {
+    "revision": "ba03f76d48f373a1b738",
+    "url": "/covid19tracker/static/js/2.5eda6e1d.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19tracker/static/js/2.5eda6e1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d15b95fcfc49524b019",
+    "url": "/covid19tracker/static/js/main.61b1ead6.chunk.js"
+  },
+  {
+    "revision": "680945e9cd32cafccde4",
+    "url": "/covid19tracker/static/js/runtime-main.f622bb78.js"
+  }
+]);
